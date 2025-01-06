e46b4b582a3ac556ffe7d04b96e2cdac45e088bd
x thang <x_thang@example.com>
2025-01-06 21:14:48 +0700

    detele route /huynguyen

--- server.js
+++ server.js
@@ -49,11 +49,7 @@ app.get("/", (req, res) => {
 });
 
 
-app.get("/huynguyen", (req, res) => {
-  res.json({
-    message: "Huy Nguyen",
-  });
-});
+
 
 const port = 5000;
 const portSocket = 3000;
